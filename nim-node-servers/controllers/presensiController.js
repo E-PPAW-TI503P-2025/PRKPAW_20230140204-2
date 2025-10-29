4f7f8d0d27ceeeb0360cf4d3f9bd32605f9d369b
hendr1an <hendriansyahgyan@gmail.com>
2025-10-29 09:36:06 +0700

    membuat endpoint baru untuk menghapus data presensi

--- nim-node-servers/controllers/presensiController.js
+++ nim-node-servers/controllers/presensiController.js
@@ -87,3 +87,30 @@
  	    res.status(500).json({ message: "Terjadi kesalahan pada server", error: error.message });
  	  }
  	};
+
+	exports.deletePresensi = async (req, res) => {
+  try {
+    const { id: userId } = req.user;
+    const presensiId = req.params.id;
+    const recordToDelete = await Presensi.findByPk(presensiId);
+
+    if (!recordToDelete) {
+      return res
+        .status(404)
+        .json({ message: "Catatan presensi tidak ditemukan." });
+    }
+
+    if (recordToDelete.userId !== userId) {
+      return res
+        .status(403)
+        .json({ message: "Akses ditolak: Anda bukan pemilik catatan ini." });
+    }
+
+    await recordToDelete.destroy();
+    res.status(204).send();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan pada server", error: error.message });
+  }
+};
